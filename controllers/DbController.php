d308453725654f4eaffdfcd9909abd6ac8f4fc43
Serik <serik.shaikamalov@gmail.com>
2018-02-14 23:41:54 +0600

    WorkWithDB

--- controllers/DbController.php
+++ controllers/DbController.php
@@ -1,21 +1,68 @@
 <?php
 
-namespace  Controllers;
+namespace  app\controllers;
 
-use \Yii;
-use \Yii\web\Controller;
+use Yii;
+use yii\web\Controller;
 
 
-public class DbController extends Controller
+class DbController extends Controller
 {
     public $defaultAction = 'home';
 
 
-    public function actionHello()
-    {
 
-        return 1;
+    // return Response object
+    // By default returns HTML response
+    public function actionUsers()
+    {
+        $dbObject = Yii::$app->db;
+
+        $users = $dbObject->createCommand('Select * FROM users')->queryAll();
+
+        $response = Yii::$app->response;
+        $response->format = 'json';
+        $response->data = $users;
+        return $response;
     }
+
+
+    public function actionUser()
+    {
+        $dbObject = Yii::$app->db;
+
+        $user = $dbObject->createCommand('Select * FROM users WHERE Id=1')->query();
+
+        $response = Yii::$app->response;
+        $response->format = 'json';
+        $response->data = $user;
+
+        return $response;
+    }
+
+
+    // Return user count
+    public function actionUserCount()
+    {
+        $dbObject = Yii::$app->db;
+
+        $userCount = $dbObject->createCommand('Select COUNT(*) FROM users')->queryScalar();
+
+        $response = Yii::$app->response;
+        $response->format = 'json';
+        $response->data = $userCount;
+
+        return $response;
+    }
+
+
+
+
+
+
+
+
+
 
 
 }
