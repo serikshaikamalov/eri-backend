454329b55548434e1ae81cd1fd55e6e8aaa3cf2b
Serik <serik.shaikamalov@gmail.com>
2018-02-27 14:38:21 +0600

    REMOVE_WEB_FROM_URL

--- config/web.php
+++ config/web.php
@@ -15,6 +15,7 @@ $config = [
     ],
     'components' => [
         'request' => [
+            'baseUrl' => '',
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
             'cookieValidationKey' => 'Serik',
         ],
